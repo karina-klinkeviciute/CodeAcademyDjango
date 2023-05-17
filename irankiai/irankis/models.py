6988600b2851d7c40cbb0f38c7c9774e96fadd25
karina <karina.klinkeviciute@.>
2023-05-17 20:34:38 +0300

    prid4tos kategorijos

--- irankiai/irankis/models.py
+++ irankiai/irankis/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Kategorija(models.Model):
+    pavadinimas = models.CharField(max_length=255)
+
+
 class Irankis(models.Model):
     pavadinimas = models.CharField(max_length=255)
     aprasymas = models.TextField(blank=True, null=True)
     galia = models.IntegerField(blank=True, null=True)
     pristatymas = models.BooleanField()
+    kategorijos = models.ManyToManyField(Kategorija)
+
+
+
+
 
 class NuomosFaktas(models.Model):
     irankis = models.ForeignKey(Irankis, on_delete=models.SET_NULL, blank=True, null=True)
