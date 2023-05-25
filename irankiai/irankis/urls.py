01efd430292490b1c4263cc4ecd64281d61e3919
karina <karina.klinkeviciute@.>
2023-05-25 18:54:42 +0300

    irankiu saraso view

--- irankiai/irankis/urls.py
+++ irankiai/irankis/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from irankis.views import index
+from naudotojo_profilis.views import IrankiaiView
 
 urlpatterns = [
     # 5ranki7 nuorodos
     path("", index, name="index"),
+    path("visi/", IrankiaiView.as_view(), name="irankis-list")
 ]
--- irankiai/naudotojo_profilis/views.py
+++ irankiai/naudotojo_profilis/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from irankis.models import Irankis
 from naudotojo_profilis.models import NaudotojoProfilis
 
 
@@ -16,3 +18,8 @@ def naudotojai(request):
 def naudotojas(request, naudotojo_id):
     naudotojo_info = NaudotojoProfilis.objects.get(pk=naudotojo_id)
     return render(request, "naudotojas.html", context={"naudotojas": naudotojo_info})
+
+
+class IrankiaiView(ListView):
+    model = Irankis
+    template_name = "irankiai.html"
