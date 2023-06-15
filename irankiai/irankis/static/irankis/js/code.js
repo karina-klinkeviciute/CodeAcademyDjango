c971fa442d900a5fef2e1faa04cad8555e804536
karina <karina.klinkeviciute@.>
2023-06-15 21:07:29 +0300

    javascript

---
+++ irankiai/irankis/static/irankis/js/code.js
@@ -0,0 +1,11 @@
+// let atsiliepimas = document.getElementById("id_atsiliepimas")
+//
+// let atsiliepimas_preview = document.getElementById("atsiliepimas_preview")
+//
+// atsiliepimas.onchange = function(){
+//     atsiliepimas_preview.innerText = atsiliepimas.innerText
+// }
+
+document.getElementById("test").style.color = "blue";
+document.getElementById("test").style.fontFamily = "Arial";
+document.getElementById("test").style.fontSize = "larger";
