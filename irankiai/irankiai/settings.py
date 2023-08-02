40f141a4d41722f1712632622b0133adc3f24206
karina <karina.klinkeviciute@.>
2023-08-02 19:47:50 +0300

    debug setting

--- irankiai/irankiai/settings.py
+++ irankiai/irankiai/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True if os.getenv("DEBUG") == "True" else False
-DEBUG = False
+DEBUG = True if os.getenv("DEBUG") == "True" else False
 ALLOWED_HOSTS = ['*']
 
 
