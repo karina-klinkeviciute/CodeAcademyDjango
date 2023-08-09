84d37ca342c511e581aa96297c8c5981cdda286b
karina <karina.klinkeviciute@.>
2023-08-09 12:39:57 +0300

    dockerization

--- irankiai/irankiai/settings.py
+++ irankiai/irankiai/settings.py
@@ -92,12 +92,21 @@ WSGI_APPLICATION = 'irankiai.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'irankiai',
+    #     'USER': 'username',
+    #     'PASSWORD': 'secret',
+    #     'HOST': 'db',  # docker nustatymai
+    # }
 }
+
 
 
 # Password validation
